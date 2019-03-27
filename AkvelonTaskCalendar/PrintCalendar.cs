12441cb69f1b0288eb032f60c8e9b83ffeda266c
Antonio2404 <azorinn@list.ru>
2019-03-27 21:53:06 +0300

    Add files via upload

---
+++ AkvelonTaskCalendar/PrintCalendar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkvelonTaskCalendar
+{
+    public class PrintCalendar
+    {
+        public int dayMomth;
+        public byte nowDay;
+        public int OneDayOfWeek;
+
+        public void OutCalendar(int dayMomth, byte nowDay, int OneDayOfWeek)
+        {
+            Console.SetCursorPosition(0, 2);
+            Console.WriteLine($"mon\ntue\nwed\nthu\nfri\nsat\nsun\n");
+
+            // i - дни месяца, j - отсчет дней недели
+            for (int i = 1, j = OneDayOfWeek, x = 6, y = 2 + (OneDayOfWeek - 1); i <= dayMomth; i++)
+            {
+                if (i == nowDay)
+                {
+                    // выделение дня
+                    Console.SetCursorPosition(x - 1, y);
+                    Console.Write($"[{i}]");
+                    j++;
+                    y++;
+
+                }
+                else
+                {
+                    //печать обычного дня
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(i);
+                    j++;
+                    y++;
+                }
+
+                //переход на следующую неделю
+                if (j == 8)
+                {
+                    j = 1;
+                    x += 4;
+                    y = 2;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ AkvelonTaskCalendar/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkvelonTaskCalendar
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //DateExep Exep = new DateExep();
+            PrintCalendar PrCalendar = new PrintCalendar();
+
+            string[] split = Console.ReadLine().Split('.', ',', '/', ' ');
+
+            try
+            {
+                SplEx(split.Length);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Не Верно введена дата!");
+                Environment.Exit(0);
+            }
+            
+            Iskl(split[0], split[1], split[2]);
+
+            // определение количества дней в месяце
+            int dayMonth = DateTime.DaysInMonth(int.Parse(split[2]), byte.Parse(split[1]));
+
+            // определение с какого дня недели по счету начинается месяц
+            DateTime dt = new DateTime(int.Parse(split[2]), byte.Parse(split[1]), 1);
+            int OneDayOfWeek = (int)dt.DayOfWeek;
+
+            // назначение sunday седьмым днем
+            if (OneDayOfWeek == 0)
+                OneDayOfWeek += 7;
+
+            PrCalendar.OutCalendar(dayMonth, byte.Parse(split[0]), OneDayOfWeek);
+
+            Console.SetCursorPosition(0,10);
+
+        }
+
+        public static void Iskl(string _DayExep, string _MonthExep, string _YearExep)
+        {
+            try
+            {
+                byte DayExep = byte.Parse(_DayExep);
+                byte MonthExep = byte.Parse(_MonthExep);
+                int YearExep = int.Parse(_YearExep);
+
+                ExDate(DayExep, MonthExep, YearExep);
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Не верный тип данных!");
+                Environment.Exit(0);
+            }
+
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Не верный диапазон даты!");
+                Environment.Exit(0);
+            }
+
+        }
+        public static void ExDate(byte day, byte month, int year)
+        {
+            if (day <= 0 || day > 31)
+                throw new ArgumentNullException();
+            if (month <= 0 || month > 12)
+                throw new ArgumentNullException();
+            if (year < 1600 || year > 2400)
+                throw new ArgumentNullException();
+        }
+
+        public static void SplEx(int a)
+        {
+            if (a < 3 || a > 3)
+                throw new ArgumentNullException();
+        }
+
+    }
+}
